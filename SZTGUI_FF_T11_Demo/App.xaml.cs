e69705c3d1f69978b209ffdfb8956529b86847fc
Szendrődi Szabolcs <szendrodiszabolcs@gmail.com>
2022-05-01 19:24:30 +0200

    Add IOC logic to App.xaml class

--- SZTGUI_FF_T11_Demo/App.xaml.cs
+++ SZTGUI_FF_T11_Demo/App.xaml.cs
@@ -1,17 +1,31 @@
-﻿using System;
+﻿using CommonServiceLocator;
+using GalaSoft.MvvmLight.Ioc;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using SZTGUI_FF_T11_Logic;
 
 namespace SZTGUI_FF_T11_Demo
 {
+    class MyIOC : SimpleIoc, IServiceLocator
+    {
+        public static MyIOC Instance { get; private set; } = new MyIOC();
+    }
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            ServiceLocator.SetLocatorProvider(() => MyIOC.Instance);
+
+            MyIOC.Instance.Register<ILoadAndSaveLogic, LoadAndSaveLogic>();
+           
+        }
     }
 }
