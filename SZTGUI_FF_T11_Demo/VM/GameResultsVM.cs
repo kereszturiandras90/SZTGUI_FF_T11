7492d43edf0635c32800b95d06356ddc86626a83
Szendrődi Szabolcs <szendrodiszabolcs@gmail.com>
2022-05-01 22:05:24 +0200

    Add View Models

---
+++ SZTGUI_FF_T11_Demo/VM/GameResultsVM.cs
@@ -0,0 +1,55 @@
+﻿using CommonServiceLocator;
+using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SZTGUI_FF_T11_CORE.Models;
+using SZTGUI_FF_T11_Logic;
+
+namespace SZTGUI_FF_T11_Demo.VM
+{
+    class GameResultsVM: ViewModelBase
+    {
+        ILoadAndSaveLogic logic;
+        
+
+        public ObservableCollection<GameResult> Results { get; private set; }
+
+        public GameResultsVM(ILoadAndSaveLogic logic)
+        {
+            this.logic = logic;
+
+            var listResults = this.logic.LoadGameResults();
+            var orderedListResults = listResults.OrderByDescending(x => x.Score).ToList();
+
+            Results = new ObservableCollection<GameResult>(orderedListResults);
+                        
+                        
+            if (IsInDesignMode)
+            {
+                GameResult gameResult1 = new GameResult { PlayerName = "VM Jani", Score = 12, DateTime = DateTime.Now };
+                GameResult gameResult2 = new GameResult { PlayerName = "VM Juli", Score = 38, DateTime = DateTime.Now.AddHours(-1) };
+
+                Results.Add(gameResult1);
+                Results.Add(gameResult2);
+            }
+        }
+
+        public GameResultsVM()
+           // : this(IsInDesignModeStatic ? null :ServiceLocator.Current.GetInstance<IGameResultLogic>())
+        {
+            
+            LoadAndSaveLogic logic = new LoadAndSaveLogic();
+
+            var listResults = logic.LoadGameResults();
+            var orderedListResults = listResults.OrderByDescending(x => x.Score).ToList();
+
+            Results = new ObservableCollection<GameResult>(orderedListResults);
+            
+        }
+
+    }
+}
