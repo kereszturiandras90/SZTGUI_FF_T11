4a72818d6ecadd0e14267667240e6217de39126e
Szendrődi Szabolcs <szendrodiszabolcs@gmail.com>
2022-05-01 22:05:03 +0200

    Add Game Menus

---
+++ SZTGUI_FF_T11_Demo/GameResults.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using SZTGUI_FF_T11_Demo.VM;
+
+namespace SZTGUI_FF_T11_Demo
+{
+    /// <summary>
+    /// Interaction logic for GameResults.xaml
+    /// </summary>
+    public partial class GameResults : Window
+    {
+        //GameResultsVM VM;
+
+        public GameResults()
+        {
+            InitializeComponent();
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SZTGUI_FF_T11_Demo/StartMenu.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SZTGUI_FF_T11_Demo
+{
+    /// <summary>
+    /// Interaction logic for StartMenu.xaml
+    /// </summary>
+    public partial class StartMenu : Window
+    {
+        public StartMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
