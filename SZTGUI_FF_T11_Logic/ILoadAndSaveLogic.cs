438a608e64aedda9880bf51a481f394f0717e41f
Szendrődi Szabolcs <szendrodiszabolcs@gmail.com>
2022-05-01 19:49:38 +0200

    Add ILoadAndSaveLogic interface

---
+++ SZTGUI_FF_T11_Logic/ILoadAndSaveLogic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SZTGUI_FF_T11_CORE.Models;
+using SZTGUI_FF_T11_CORE.Settings;
+
+namespace SZTGUI_FF_T11_Logic
+{
+    public interface ILoadAndSaveLogic
+    {
+        public IList<GameResult> LoadGameResults();
+
+        public GameModel LoadGameModel(string path);
+
+        public GameSettings LoadGameSettings(string path);
+
+        public void SaveGameResult(GameResult gameResult);
+
+        public void SaveGameModel(GameModel gameModel, string path);
+
+        public void SaveGameSettings(GameSettings gameSettings, string path);
+
+    }
+}
